52ed982aa260175c5bb1c4ea89261cbfed0bceea
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-23 12:07:36 +0200

    install dotenv

--- Backend/App.js
+++ Backend/App.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
 app.get('/home', (req, res) => {
     res.send('Hello Home!');
 });
