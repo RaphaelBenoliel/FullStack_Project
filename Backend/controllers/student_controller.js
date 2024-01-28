78dba8961a58b8a14e2bb712365d69394989c72a
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-28 20:34:01 +0200

    create getusuerbyID

--- Backend/controllers/student_controller.js
+++ Backend/controllers/student_controller.js
@@ -1,31 +1,52 @@
 const Student = require("../models/student_model");
 
-const getStudents = (req, res) => {
-    res.send("student get");
-  };
-  const getStudentById = (req, res) => {
-    console.log(req.params);
-    res.send("student get by id");
-  };
-  
-  const postStudents = async (req, res) => {
-    console.log("student post");
-    try {
-      const student = await Student.create(req.body);
-      res.status(201).send(student);
-    } catch(error){
-        console.log(error);
-        res.status(400).send(error.message);
+const getStudents = async (req, res) => {
+  console.log("student get");
+  try {
+    let student;
+    if (req.query.name) {
+      student = await Student.find({ name: req.query.name });
+    } else {
+      student = await Student.find();
     }
-    
-  };
+    res.status(200).send(student);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error.message);
+  }
+};
+
+
+const getStudentById = async (req, res) => {
+  console.log(req.params);
+  try {
+    const student = await Student.findById(req.params.id);
+    res.status(200).send(student);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error.message);
+  }
+};
+
+
+const postStudents = async (req, res) => {
+  console.log("student post");
+  try {
+    const student = await Student.create(req.body);
+    res.status(201).send(student);
+  } catch(error){
+      console.log(error);
+      res.status(400).send(error.message);
+  }
   
-  const putStudents = (req, res) => {
-    res.send("student put");
-  };
+};
   
-  const deleteStudents = (req, res) => {
-    res.send("student delete");
-  };
-  
-  module.exports = { getStudents, postStudents, putStudents, deleteStudents, getStudentById };
+const putStudents = (req, res) => {
+  res.send("student put");
+};
+
+const deleteStudents = (req, res) => {
+  res.send("student delete");
+};
+
+module.exports = { getStudents, postStudents, putStudents, deleteStudents, getStudentById };
