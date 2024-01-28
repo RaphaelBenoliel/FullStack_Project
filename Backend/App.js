1f6d52b02299472bc0c1f2f6f5ddd87bbeab2340
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-28 16:09:39 +0200

    body-parser imstal and create student on db

--- Backend/App.js
+++ Backend/App.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT
@@ -12,14 +13,12 @@ const db = mongoose.connection;
 db.on('error', (err) =>console.log(err));
 db.once('open', () => console.log('Connected to DB'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/student', studentRoute);
 app.use('/post', postRoute);
 
-
-
-
 app.listen(port, () => {
     console.log(`Example app listening at  http://localhost:${port}`);
 });
-
--- Backend/controllers/student_controller.js
+++ Backend/controllers/student_controller.js
@@ -1,9 +1,23 @@
+const Student = require("../models/student_model");
+
 const getStudents = (req, res) => {
     res.send("student get");
   };
+  const getStudentById = (req, res) => {
+    console.log(req.params);
+    res.send("student get by id");
+  };
   
-  const postStudents = (req, res) => {
-    res.send("student post");
+  const postStudents = async (req, res) => {
+    console.log("student post");
+    try {
+      const student = await Student.create(req.body);
+      res.status(201).send(student);
+    } catch(error){
+        console.log(error);
+        res.status(400).send(error.message);
+    }
+    
   };
   
   const putStudents = (req, res) => {
@@ -14,4 +28,4 @@ const getStudents = (req, res) => {
     res.send("student delete");
   };
   
-  module.exports = { getStudents, postStudents, putStudents, deleteStudents };
+  module.exports = { getStudents, postStudents, putStudents, deleteStudents, getStudentById };
