9cdb9fe014d793ee4946ddd9cbd03ac7d35ff5ee
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-28 15:15:28 +0200

    connect db and build schema

--- Backend/App.js
+++ Backend/App.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT
 
 const studentRoute = require('./routes/student_route');
-app.use('/student', studentRoute);
 const postRoute = require('./routes/post_route');
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (err) =>console.log(err));
+db.once('open', () => console.log('Connected to DB'));
+
+
+app.use('/student', studentRoute);
 app.use('/post', postRoute);
 
 
