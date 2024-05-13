357d4a453358e41434bb7cee38e57b46feb0b859
Raphael Benoliel <raphael2gb@gmail.com>
2024-05-13 20:24:50 +0300

    ios and  android build

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -5,17 +5,47 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import StudentListPage from './components/StudentListPage';
 import StudentDetailsPage from './components/StudentDetailsPage';
 import StudentAddPage from './components/StudentAddPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Ionicons } from '@expo/vector-icons'; // A
+import GoogleSignIn from './components/GoogleSignIn';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const StudentsListStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+// 1023628505389-r4ptl499nh99vpok1u2erkdi1gh9g75i.apps.googleusercontent.com
+const AuthStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const HomeScreen: FC = () => {
+  return(
+    <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: '#0d1117',
+          },
+          tabBarActiveTintColor: '#fff',
+          tabBarInactiveTintColor: '#8b949e',
+        }}
+       >
+        <Tab.Screen name="StudentsListScreen" component={StudentsListScreen} options={{ headerShown: false,  }} />
+        <Tab.Screen name="StudentAddPage" component={StudentAddPage} options={{ title: 'Add New Student'
+      //   , headerStyle: {
+      //   backgroundColor: '#0d1117',
+      // },
+      // headerTintColor: '#fff', 
+      // headerTitleStyle: {
+      //   fontWeight: 'bold',
+      // }
+     }} />
+      </Tab.Navigator>
+  )
 
-
+}
 const StudentsListScreen: FC = () => {
   return (
-    <StudentsListStack.Navigator
+    <StudentsListStack.Navigator 
     screenOptions={
       {headerStyle: {
         backgroundColor: '#0d1117',
@@ -33,28 +63,32 @@ const StudentsListScreen: FC = () => {
 }
 
 export default function App() {
+  const [auth, setAuth] = useState(false);
+
+  const fetch = async () => {
+    const authToken = await AsyncStorage.getItem('token');
+    if (authToken) {
+      setAuth(true);
+    }
+
+
+    
+  }
+  fetch();
+
 
   return (
     <NavigationContainer>
-       <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: '#0d1117',
-          },
-          tabBarActiveTintColor: '#fff',
-          tabBarInactiveTintColor: '#8b949e',
-        }}
-       >
-        <Tab.Screen name="StudentsListScreen" component={StudentsListScreen} options={{ headerShown: false,  }} />
-        <Tab.Screen name="StudentAddPage" component={StudentAddPage} options={{ title: 'Add New Student', headerStyle: {
-        backgroundColor: '#0d1117',
-      },
-      headerTintColor: '#fff', 
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      }
-     }} />
-      </Tab.Navigator>
+      <AuthStack.Navigator>
+        {auth ? (
+          <AuthStack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+        ) : (
+          <>
+            <AuthStack.Screen name="LoginPage" component={LoginPage} options={{ headerShown: false }} />
+            <AuthStack.Screen name="RegisterPage" component={RegisterPage} options={{ headerShown: false }} />
+          </>
+        )}
+      </AuthStack.Navigator>
     </NavigationContainer>
   );
 }
